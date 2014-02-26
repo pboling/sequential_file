4e01f0f6491bcdfbc02bebdde3309b0aca96a63e
Peter Boling <peter.boling@gmail.com>
2014-02-26 10:16:20 -0500

    Tests for CounterFinder

--- lib/sequential_file/counter_finder.rb
+++ lib/sequential_file/counter_finder.rb
@@ -14,14 +14,14 @@ module SequentialFile
       @index_of_extension_separator = @filename.rindex('.')
     end
 
-    def has_counter?
+    def has_extension_separator?
       !!@index_of_extension_separator
     end
 
     # returns:
     #   an integer representation of the counter
     def counter
-      has_counter? ?
+      has_extension_separator? ?
         @filename[@index_of_extension_separator + 1,@length][NUMBER_REGEX].to_i :
         0
     end
